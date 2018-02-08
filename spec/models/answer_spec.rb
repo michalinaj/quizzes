8d4278ee8d5f35ed6f134f1f901115726db21599
michalinaj <michalina@jablonska.me>
2018-02-08 18:52:18 -0500

    fixed tests issues

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,5 +4,4 @@ require 'rails_helper'
     it {should have_valid(:body).when('Ola')}
     it {should_not have_valid(:body).when(nil, '', ' ')}
 
-  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,5 +4,4 @@ require 'rails_helper'
     it {should have_valid(:body).when('Hello in Spanish')}
     it {should_not have_valid(:body).when(nil, '', ' ')}
 
-  end
 end
