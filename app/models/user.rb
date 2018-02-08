0f09f876c736c0480aaa4873d7f21d8a4a8535e7
michalinaj <34382310+michalinaj@users.noreply.github.com>
2018-02-08 18:44:51 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mount_uploader :profile_photo, ProfilePhotoUploader
-
   validates :username, presence: true, uniqueness: true
 end
