2a3d8e98bb87bb7a4e23bf18da149fe54a613a6c
michalinaj <michalina@jablonska.me>
2018-02-07 20:10:31 -0500

    added question feature and tests

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+  RSpec.describe Question, type: :model do
+    it {should have_valid(:body).when('Hello in Spanish')}
+    it {should_not have_valid(:body).when(nil, '', ' ')}
+
+    end}
+  end
+end
