666277e90d532e20e9f0e1fedc06541eefa02193
michalinaj <michalina@jablonska.me>
2018-02-10 19:42:38 -0500

    added category model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
   RSpec.describe Answer, type: :model do
     it {should have_valid(:body).when('Ola')}
     it {should_not have_valid(:body).when(nil, '', ' ')}
-
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
   RSpec.describe Question, type: :model do
     it {should have_valid(:body).when('Hello in Spanish')}
     it {should_not have_valid(:body).when(nil, '', ' ')}
-
+  end
 end
